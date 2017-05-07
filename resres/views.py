78fe623b22c1180b7f10f2dd0ebb57b9749bb9d8
Morten Vester Pedersen <mortenvester1@gmail.com>
2017-05-07 16:11:34 -0400

    templates

--- resres/views.py
+++ resres/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Users, Resource, Reservation
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'index.html')
+    #return HttpResponse("Hello, world.\n"+s)
+
+def addUser(request):
+    return HttpResponse('User Added')
